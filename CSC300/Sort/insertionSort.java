064ca70c3ba6c3e72276279ee14bb6b54307b202
Mikala Metzger <mmetzgerdpu2021@gmail.com>
2020-11-05 20:56:57 -0600

    10/10 done.

--- CSC300/Sort/insertionSort.java
+++ CSC300/Sort/insertionSort.java
@@ -1,15 +1,21 @@
-// Title: Insertion Sort
-// Original author: Luis
-
-// Question
-/*
-*/
-
-// Answer
+/**
+ * Sort a list by moving values into place one-by-one in order.
+ *
+ * @author Luis
+ *
+ * @param a Container that needs to be sorted.
+ *
+ * @return void Changes persist through pass by reference.
+ */
 public static void insertionSort (Comparable[] a) {
-  int n = a.length;
-  for (int i=0; i<n; i++) {
-    for (int j=i; j>0; j--) {
-      if (less(a[j], a[j-1])) {
-        exch(a, j, j-1);
-}  }  }  }
+
+  for (int i=0; i<a.length; i++) {  // for each value in the array
+    for (int j=i; j>0; j--) {       // move the value left
+
+      if (less(a[j], a[j-1])) {     // If the current val is less than the
+                                    // value to the left
+        exch(a, j, j-1);            // exhange the two values
+      }
+    }
+  }
+}
